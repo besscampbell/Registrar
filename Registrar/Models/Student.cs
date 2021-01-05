0b44baa08bba66a009481ca41b062224569b08eb
Bess Campbell <bess.k.campbell@gmail.com>
2021-01-05 10:34:43 -0800

    Add join table to models

--- Registrar/Models/Student.cs
+++ Registrar/Models/Student.cs
@@ -1,15 +1,22 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System;
+using System.Collections.Generic;
 
 namespace Registrar.Models
 {
     public class Student
     {
+        public Student()
+        {
+            this.Courses = new HashSet<CourseStudent>();
+        }
         public int StudentId { get; set; }
         public string Name { get; set; }
         [DisplayName("EnrollmentDate")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM-dd-yyyy}")]
         public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<CourseStudent> Courses { get; }
     }
 }
