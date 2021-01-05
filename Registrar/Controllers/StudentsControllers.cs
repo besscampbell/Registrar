d9ce5a4b190c49b0ecdb6e81ccb9bcde3cbd290d
Bess Campbell <bess.k.campbell@gmail.com>
2021-01-05 11:49:29 -0800

    Fully integrate courses and students
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

--- Registrar/Controllers/StudentsControllers.cs
+++ Registrar/Controllers/StudentsControllers.cs
@@ -23,32 +23,45 @@ namespace Registrar.Controllers
 
         public ActionResult Create()
         {
+            ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
             return View();
         }
 
         [HttpPost]
-        public ActionResult Create(Student student)
+        public ActionResult Create(Student student, int CourseId)
         {
             _db.Students.Add(student);
+            if (CourseId != 0)
+            {
+                _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId});
+            }
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
 
         public ActionResult Details(int id)
         {
-            var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+            var thisStudent = _db.Students
+                .Include(student => student.Courses)
+                .ThenInclude(join => join.Course)
+                .FirstOrDefault(student => student.StudentId == id);
             return View(thisStudent);
         }
 
         public ActionResult Edit(int id)
         {
             var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+            ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "Name");
             return View(thisStudent);
         }
 
         [HttpPost]
-        public ActionResult Edit(Student student)
+        public ActionResult Edit(Student student, int CourseId)
         {
+            if (CourseId != 0)
+            {
+                _db.CourseStudent.Add(new CourseStudent() { CourseId = CourseId, StudentId = student.StudentId});
+            }
             _db.Entry(student).State = EntityState.Modified;
             _db.SaveChanges();
             return RedirectToAction("Index");
@@ -65,6 +78,15 @@ namespace Registrar.Controllers
         {
             var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
             _db.Students.Remove(thisStudent);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteCourse(int joinId)
+        {
+            var joinEntry = _db.CourseStudent.FirstOrDefault(entry => entry.CourseStudentId == joinId);
+            _db.CourseStudent.Remove(joinEntry);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
--- Registrar/Models/RegistrarContext.cs
+++ Registrar/Models/RegistrarContext.cs
@@ -4,8 +4,9 @@ namespace Registrar.Models
 {
   public class RegistrarContext : DbContext
   {
-    public DbSet<Course> Courses { get; set; }
+    public virtual DbSet<Course> Courses { get; set; }
     public DbSet<Student> Students { get; set; }
+    public DbSet<CourseStudent> CourseStudent { get; set; }
     public RegistrarContext(DbContextOptions options) : base(options) { }
   }
 }
